123cbd24df00f33668b7667a12ad3c6e5e0e8ba3
makeworld <makeworld@protonmail.com>
2018-12-22 17:04:36 -0500

    Works

--- README.md
+++ README.md
@@ -2,14 +2,12 @@
 Reads out news headlines from r/worldnews on reddit
 
 ## Description
-This skill scrapes from r/worldnews using a get request, and then has mycroft read up the headlines.
+This skill scrapes from r/worldnews using a GET request, and then has mycroft read up the headlines.
 
 ## Examples
- - "What's the news"
- - "Tell me the news"
- - "Play the news"
  - "Tell me the news from reddit"
  - "What's the news from reddit"
+ - Any phrase with news and reddit in it
 
 
 ## Credits
--- __init__.py
+++ __init__.py
@@ -1,13 +1,27 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
+from adapt.intent import IntentBuilder
+import requests
+import json
 
 
 class RedditNews(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('news.reddit.intent')
+    @intent_handler(IntentBuilder("").optionally("Pre").require("News").require("Reddit"))
     def handle_news_reddit(self, message):
-        self.speak_dialog('news.reddit')
+        r = requests.get("https://reddit.com/r/worldnews/hot/.json", headers={"User-Agent": "mycroft:reddit-news-skill:v0.1 (by /u/makeworld)"})
+        data = r.json()
+        headlines = ""
+        for post in data["data"]["children"][:5]:  # First five posts
+            headline = post["data"]["title"]
+            if headline[-1:] != ".":  # Add a period at the end to make mycroft pause
+                headline += "."
+            # Add a space at the end, because all headlines will be in one string
+            headline += " "
+            # Add it to the string of headlines
+            headlines += headline
+        self.speak_dialog('here.news', data={"headlines": headlines})
 
 
 def create_skill():
